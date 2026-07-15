73dafbd2ee643ba4573530136b83bbae7a57e71e
Roman Bashmakov <bashmakovra@raitibor.ru>
2026-07-15 21:43:16 +0300

    Второй пин переключается. Убрал лишний overlay. Добавил app.overlay.

--- src/main.c
+++ src/main.c
@@ -13,12 +13,14 @@
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 
 int main(void)
 {
@@ -33,9 +35,17 @@ int main(void)
 	if (ret < 0) {
 		return 0;
 	}
+    ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+		return 0;
+    }
 
 	while (1) {
 		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return 0;
+		}
+		ret = gpio_pin_toggle_dt(&led1);
 		if (ret < 0) {
 			return 0;
 		}
